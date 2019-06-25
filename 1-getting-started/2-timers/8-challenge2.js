753efd361957618ce455a3d8b27b5eb051804518
EricHeredia <EricDavidHeredia@gmail.com>
2019-06-25 17:56:36 -0500

    challenge2 complete

--- 1-getting-started/2-timers/8-challenge2.js
+++ 1-getting-started/2-timers/8-challenge2.js
@@ -3,3 +3,14 @@
 // And stop after 5 times
 
 // After 5 times. Print "Done" and let Node exit.
+
+let count = 0
+let helloLoop = setInterval(() => {
+  if (count < 5) {
+    console.log("Hello World")
+    count++
+  } else {
+    console.log("Done")
+    clearInterval(helloLoop)
+  }
+}, 1000)
